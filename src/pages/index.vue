c97946f8e1683194ee2d592b240ff9826a0dcd6b
watsuyo <dmwtpdmwtp24@gmail.com>
2019-10-27 19:40:07 +0900

    fix: get url

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,26 @@
 <template>
   <div>
-    Hello World
+    <div>
+      <input type="text" :value="url" />
+    </div>
+    <div>
+      <input type="text" :value="title" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  mounted() {
-    const url = location.href
-    const title = document.title
-    console.log(title + '\n' + url)
+  computed: {
+    url() {
+      const url = process.browser && location.href
+      return url
+    },
+    title() {
+      const title = process.browser && document.title
+      return title
+    }
   }
 })
 </script>
