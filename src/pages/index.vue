4aad32a3776406c26e4a4b300401ea5e83093fac
watsuyo <dmwtpdmwtp24@gmail.com>
2019-11-09 13:57:46 +0900

    add: twitter button

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,24 @@
     <div class="title">
       thanks-mentions
     </div>
+    <div class="share">
+      thanks-mentionsをシェア
+      <div>
+        <a
+          href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+          class="twitter-share-button"
+          data-text="Qiitaの記事をメンション付きで共有できるPWA"
+          data-url="http://thanks-mentions.web.app/"
+          data-via="watsuyo_2"
+          data-hashtags="thanks_mentions"
+          data-related="watsuyo_2,watsuyo_2"
+          data-dnt="true"
+          data-show-count="false"
+        >
+          Twitterでシェア
+        </a>
+      </div>
+    </div>
     <div class="subtitle">
       <p>Qiitaの記事をメンション付きで共有できるPWAアプリ</p>
     </div>
@@ -57,6 +75,25 @@
         src="https://firebasestorage.googleapis.com/v0/b/thanks-mentions.appspot.com/o/tweet.jpg?alt=media&token=43e7f995-5b7b-4762-af92-3e2f325c3fb3"
       />
     </div>
+    <a
+      href="https://twitter.com/intent/tweet?screen_name=watsuyo_2&ref_src=twsrc%5Etfw"
+      class="twitter-mention-button"
+      data-size="large"
+      data-text="Qiitaの記事をメンション付きで共有できるPWA"
+      data-url="http://thanks-mentions.web.app/"
+      data-via="watsuyo_2"
+      data-hashtags="thanks_mentions"
+      data-related="watsuyo_2,watsuyo_2"
+      data-dnt="true"
+      data-show-count="false"
+    >
+      Tweet to @watsuyo_2
+    </a>
+    <script
+      async
+      src="https://platform.twitter.com/widgets.js"
+      charset="utf-8"
+    ></script>
   </div>
 </template>
 
@@ -68,12 +105,15 @@ export default Vue.extend({})
 
 <style lang="stylus" scoped>
 img
-  width 100%
-  height 400px
+  width 80%
+  height 80%
   object-fit cover
 
 .title
   font-size 36px
+
+.share
+  margin 50px
 
 .subtitle
   font-size 20px
@@ -87,7 +127,7 @@ img
   font-size 20px
 
 .procedure-description
-  text-align left
+  text-align center
   font-size 16px
   margin 20px 50px
 
